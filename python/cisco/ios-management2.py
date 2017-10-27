04984e574ec75cf2297adafe2f2cfe38999c0d6f
dlakey <lakesystem@gmail.com>
2017-10-27 13:47:09 -0700

    Automation Repo

--- python/cisco/ios-management2.py
+++ python/cisco/ios-management2.py
@@ -4,7 +4,7 @@ import base64
 
 host = raw_input('Enter Hostname or IP address of Device: ')
 
-commands = ['show version\n', 'show run', 'show ip interface brie']
+commands = ['show version\n', 'show run', 'show vlan']
 
 with open(host + '_show_cmd.cfg', 'w') as f:
 	for command in commands:
@@ -13,7 +13,7 @@ with open(host + '_show_cmd.cfg', 'w') as f:
 	 	ssh.connect(host, username='cisco', password='cisco', look_for_keys=False, allow_agent=False)
 	 	time.sleep(1)
 		stdin, stdout, stderr = ssh.exec_command(command)
-		time.sleep(3)
+		time.sleep(5)
 		f.write(stdout.channel.recv(8000))
 		ssh.close()
 
